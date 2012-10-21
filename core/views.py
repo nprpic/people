4c6a851f815e546afd500a3385f6a2ce25deea52
Nikolina Prpic <nina@parfem.(none)>
2012-10-21 23:14:05 +0200

    Adding and deleting users

---
+++ core/views.py
@@ -0,0 +1,74 @@
+from django.db import IntegrityError
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+import json
+
+
+def index(request):
+    return render(request, 'login.html')
+
+def login_view(request):
+	if request.method == 'POST':
+		username = request.POST.get('username','')
+		password = request.POST.get('password','')
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			login(request, user)
+			request.session['username'] = username
+			return redirect(people)
+		else:
+			messages.warning(request, 'Incorrect username or password.')
+			return redirect(index)
+
+def logout_view(request):
+	try:
+		del request.session ['username']
+	except KeyError:
+		pass
+	logout(request)
+	return redirect(index)
+
+@login_required
+def people(request):
+	all_persons = User.objects.all()
+	return render(request, 'people.html', {'persons':all_persons})
+
+@login_required
+def add_new_person(request):
+	return render(request, 'add_new_person.html')
+
+@login_required
+def save_person(request):
+	if request.method == 'POST':
+		first_name = request.POST.get('first_name','')
+		last_name = request.POST.get('last_name','')
+		username = request.POST.get('username','')
+		password = request.POST.get('password','')
+		person = User(first_name=first_name, last_name=last_name, username=username)
+		person.set_password(password)
+		try:
+			person.save()
+			return redirect(people)
+		except IntegrityError:
+			messages.warning(request, 'Username already exists!')
+			return redirect(add_new_person)
+
+@login_required
+def person_profile(request, person_id):
+	person = User.objects.get(id=person_id)
+	return render(request, 'person_profile.html', {'person':person})
+
+@login_required
+def delete_person(request, person_id):
+	person = User.objects.get(id=person_id).delete()
+	return redirect(people)
+
+def autocomplete(request):
+	all_persons = User.objects.all()
+	val = [{"label":p.first_name + " " + p.last_name, "value":p.id} for p in all_persons]
+	return HttpResponse(json.dumps(val), mimetype='application/json')
--- people/urls.py
+++ people/urls.py
@@ -9,6 +9,16 @@ urlpatterns = patterns('',
     # url(r'^$', 'skeletor.views.home', name='home'),
     # url(r'^skeletor/', include('skeletor.foo.urls')),
 
+    url(r'^$', 'core.views.index'),
+    url(r'^login$', 'core.views.login_view'),
+    url(r'^logout$', 'core.views.logout_view'),
+    url(r'^people$', 'core.views.people'),
+    url(r'^add_new_person$', 'core.views.add_new_person'),
+    url(r'^save_person$', 'core.views.save_person'),
+    url(r'^person_profile/(?P<person_id>\d+)$', 'core.views.person_profile'),
+    url(r'^delete_person/(?P<person_id>\d+)$', 'core.views.delete_person'),
+    url(r'^autocomplete$', 'core.views.autocomplete'),
+
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
