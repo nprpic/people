e15e40a2c7fa4e3edb75e5af689aaeea18f54419
Nikolina Prpic <nina@parfem.(none)>
2012-11-11 14:26:53 +0100

    add name to urls in core urls.py

--- core/urls.py
+++ core/urls.py
@@ -9,11 +9,11 @@ urlpatterns = patterns('',
     # url(r'^$', 'skeletor.views.home', name='home'),
     # url(r'^skeletor/', include('skeletor.foo.urls')),
 
-    url(r'^people$', 'core.views.people'),
-    url(r'^people/add$', 'core.views.add_new_person'),
-    url(r'^people/(?P<user_id>\d+)$', 'core.views.person_profile'),
-    url(r'^people/(?P<user_id>\d+)/delete$', 'core.views.delete_person'),
-    url(r'^autocomplete$', 'core.views.autocomplete'),
+    url(r'^people$', 'core.views.people', name='people'),
+    url(r'^people/add$', 'core.views.add_new_person', name='add_person'),
+    url(r'^people/(?P<user_id>\d+)$', 'core.views.person_profile', name='person_profile'),
+    url(r'^people/(?P<user_id>\d+)/delete$', 'core.views.delete_person', name='delete_person'),
+    url(r'^autocomplete$', 'core.views.autocomplete', name='autocomplete'),
 )
 
 # Note: this only gets activated if DEBUG = True
